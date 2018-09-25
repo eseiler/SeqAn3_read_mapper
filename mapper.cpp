c82ee7e59c3404bf879cca98f1a4c406b751bdf5
smehringer <svenja.mehringer@fu-berlin.de>
2018-09-25 09:00:52 +0200

    [NOP] include namespace on global scope.

--- mapper.cpp
+++ mapper.cpp
@@ -1,8 +1,10 @@
 #include <seqan3/io/stream/debug_stream.hpp>
 
+using namespace seqan3;
+
 int main(int argc, char const ** argv)
 {
-    using namespace seqan3;
     debug_stream << "Hello world\n";
     return 0;
 }
+
